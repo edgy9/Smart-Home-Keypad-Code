086aa6f94c78b37a1addf216fa4344198e692f6e
edgy9 <benfhewston@gmail.com>
2022-02-02 11:56:36 +0000

    update

--- keypad_client_V2/keypad_client_V2.ino
+++ keypad_client_V2/keypad_client_V2.ino
@@ -1,9 +1,70 @@
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
+  bool synced = false;
+  bool canceled_sync = false;
+  pinMode(3, INPUT);
+  pinMode(4, INPUT);
+  pinMode(5, INPUT);
+  pinMode(6, INPUT);
+  pinMode(7, INPUT);
+  pinMode(8, INPUT);
+  pinMode(9, INPUT);
+  pinMode(10, INPUT);
+  pinMode(11, INPUT);
+  pinMode(12, INPUT);
+  pinMode(2, OUTPUT);
+  digitalWrite (2, LOW );
+  int device_id = 1;
+  int device_type = 1;
+  
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 
-}
+  if ( Serial.available ()) {
+    if ( Serial.read () == 'I' ) {
+      char id = Serial.read();
+      if (id == Z){
+        char function = Serial.read ();
+        if (function == 'S' ) {
+          if ( Serial.read () == 'F' ) {
+            sync()
+          }
+        }
+      }
+      
+      if (id == device_id){
+          if (function == 'P'){
+            ping()
+          }
+        }
+    }
+    void ping() {
+      digitalWrite(2,HIGH);
+      Serial.write("i");
+      Serial.write(device_id);
+      Serial.write("p");
+      Serial.write(device_type);
+      Serial.flush();
+      digitalWrite(2,LOW);
+    }
+    void sync() {
+      if (synced == false) {
+        synced = True
+        while (canceled_sync == false) {
+          if (digitalRead(3) == HIGH) {
+             digitalWrite(2, HIGH ); //enable to transmit
+             Serial.print( "i" ); 
+             Serial.println( "s" );
+             Serial.println("n"); 
+             Serial.flush(); 
+             digitalWrite(2, LOW ); 
+          }
+          
+        }
+
+      }
+
+
+    }
--- keypad_controller_V2/keypad_controller_V2.ino
+++ keypad_controller_V2/keypad_controller_V2.ino
@@ -2,7 +2,34 @@ bool sync_button = False
 
 void setup() {
    Serial.begin(9600);
+   pinMode(2, OUTPUT);
+   digitalWrite (2, HIGH );
+   array devices = []
+   int current_id_count = 0
 
+}
+
+void ping_devices(){
+  digitalWrite(2, HIGH):
+  int i = 1
+  while (i < 17) {
+    Serial.print("I");
+    Serial.print(i):
+    Serial.print("P")
+    
+    digitalWrite(2, LOW)
+    
+    if(Serial.find("i")) {
+      if(Serial.read() =='i') {
+          if(Serial.read()=='f') //finish reading
+       {
+        
+         digitalWrite(2, HIGH)
+         new_device()           
+      }
+    
+    i = i + 1
+  }
 }
 
 void loop() {
@@ -12,5 +39,29 @@ void loop() {
 }
 
 void sync() {
-  serial
+  
+  Serial.print("I"); //initiate data packet
+  Serial.print("S"); //code for Sync
+  Serial.print("F"); //finish data packet
+  Serial.flush();    
+  
+  digitalWrite(2, LOW);
+  
+  if(Serial.find("i"))
+  {
+      if(Serial.read() =='n') {
+          if(Serial.read()=='f') //finish reading
+       {
+        
+         digitalWrite(2, HIGH)
+         new_device()           
+      }
+      
+  }
+
+}
+void new_device() {
+  Serial.print("I");
+  Serial.print("N");
+  Serial.print(id);
 }
