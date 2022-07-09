f06fc15b9f5bc8507d15b34fd11d8a110d8929d5
Ben <benfhewston@gmail.com>
2022-07-09 09:22:29 +0100

    need to fix syncing

--- keypad_controller_V3/keypad_controller_V3.ino
+++ keypad_controller_V3/keypad_controller_V3.ino
@@ -27,7 +27,10 @@ int device_state = 0;   //0 = booting, 1 = pending adoption, 2 = running
 
 const char* mqtt_device_topic = "L_S_C/";
 const char* mqtt_adoption_topic = "/adoption";
+const char* mqtt_stats_topic = "/stats";
 const char* mqtt_state_topic = "/state";
+const char* mqtt_ping_topic = "/ping";
+const char* mqtt_sync_topic = "/sync";
 
 
 byte online =     1;
@@ -39,21 +42,106 @@ byte device_list[][3] =   //id
   1,  0,  offline,
   2,  0,  offline,
   3,  0,  offline,
-  4,  0,  offline,
-  5,  0,  offline,
-  6,  0,  offline,
-  7,  0,  offline,
-  8,  0,  offline,
-  9,  0,  offline,
-  10, 0,  offline,
-  11, 0,  offline,
-  12, 0,  offline,
-  13, 0,  offline,
-  14, 0,  offline,
-  15, 0,  offline,
+
 };
 
 bool sync_button = false;
+bool ping_pending = false;
+
+void new_device() {
+  int i = 1;
+  ping_devices();
+ 
+  while(i < 17){                                                              //////////////////
+      if(device_list[i][2] == offline){
+        int id = i;
+        Serial.print('I');
+        Serial.print('N');
+        Serial.print('P');
+        Serial.print(id);
+        Serial.print('F');
+        break;
+        }
+      ping_devices();
+  }
+  
+}
+
+void sync() {
+  digitalWrite(rx_tx_pin, HIGH);
+  Serial.print('I'); //initiate data packet
+  Serial.print('Z'); //code for unassigned
+  Serial.print('S'); //code for Sync
+  Serial.print('F'); //finish data packet
+  Serial.flush();    
+  
+  digitalWrite(rx_tx_pin, LOW);
+  
+  sync_button = false;
+  return;
+  
+  if(Serial.find('i')) {
+      if(Serial.read() =='z') {
+          if(Serial.read() =='n') {
+              if(Serial.read() =='n') {
+                  if(Serial.read()=='f') //finish reading
+                      {
+                
+                         digitalWrite(rx_tx_pin, HIGH);
+                         new_device(); 
+                      }
+              } 
+          }
+      
+      }
+
+  }
+  sync_button = false;
+}
+
+void ping_devices(){
+  
+  
+  for (int i = 1; i<5; i++){
+  //while (i < 17) {                      //////////
+    digitalWrite(rx_tx_pin, HIGH);
+    Serial.print('I');
+    Serial.print(i);
+    Serial.print('P');
+    Serial.flush();  
+    digitalWrite(rx_tx_pin, LOW);
+    delay(50); 
+    if(Serial.find('i')) {
+        
+        int id = Serial.parseInt();
+        
+        if(Serial.read() == 'p'){
+          int device_type=Serial.parseInt(); 
+          if(Serial.read()=='f') //finish reading
+          {
+            digitalWrite(5, HIGH);
+            device_list[i][1] = device_type;
+            device_list[i][2] = online;
+            delay(100);
+            digitalWrite(5, LOW);
+          }
+         
+       }       
+     }
+     else{
+          device_list[i][2] = offline;
+     }
+    
+    
+  }
+  digitalWrite(rx_tx_pin, LOW);
+  Serial.println();
+  ping_pending = true;
+  
+}
+
+
+
 
 void callback(char* topic, byte* payload, unsigned int length) {
   char str[length+1];
@@ -90,10 +178,38 @@ void callback(char* topic, byte* payload, unsigned int length) {
       Serial.println(controller_uuid);
       EEPROM.write(adopted_eeprom_addr, 102);//successfully adopted
       device_state = 2;   //set state to running
+      
       }
   //}
   Serial.println();
-}
+  if(device_state == 2){
+    char test_topic[20];
+    strcpy(test_topic, mqtt_device_topic);
+    dtostrf(controller_uuid, 2,0,char_controller_uuid);
+    strcat(test_topic, char_controller_uuid);
+    strcat(test_topic, mqtt_ping_topic);
+   // sprintf(test_topic, "%s%d%s", mqtt_device_topic,controller_uuid,mqtt_ping_topic);
+    
+    if (strcmp(topic,test_topic)==0){
+      if (strcmp(str,"ping")==0){
+        //Serial.print("pinging");
+        ping_devices();
+        }
+    }
+    //sprintf(topic, "%s%d%s", mqtt_device_topic,controller_uuid,mqtt_sync_topic);
+    strcpy(test_topic, mqtt_device_topic);
+    dtostrf(controller_uuid, 2,0,char_controller_uuid);
+    strcat(test_topic, char_controller_uuid);
+    strcat(test_topic, mqtt_sync_topic);
+    if (strcmp(topic,test_topic)==0){
+      if (strcmp(str,"sync")==0){
+        Serial.print("should sync");
+        sync_button = true;
+      }
+    }
+  }
+  }
+
 
 EthernetClient ethClient;
 PubSubClient client(ethClient);
@@ -110,8 +226,9 @@ void reconnect() {
       
       char topic[38];
       strcpy(topic, mqtt_device_topic);
-      dtostrf(123, 3,0,char_controller_uuid);
+      dtostrf(controller_uuid, 2,0,char_controller_uuid);
       strcat(topic, char_controller_uuid);
+      strcat(topic, mqtt_stats_topic);
       strcat(topic, mqtt_state_topic);
       
       client.publish(topic,"Connected");
@@ -132,43 +249,7 @@ void reconnect() {
 
 
 
-void ping_devices(){
-  
-  int i = 1;
-  for (int i=0; i<17; i++){
-  //while (i < 17) {                      //////////
-    digitalWrite(rx_tx_pin, HIGH);
-    Serial.print('I');
-    Serial.print(i);
-    Serial.print('P');
-    Serial.flush();  
-    digitalWrite(rx_tx_pin, LOW);
-    delay(1000); 
-    if(Serial.find('i')) {
-        
-        int id = Serial.parseInt();
-        
-        if(Serial.read() == 'p'){
-          int device_type=Serial.parseInt(); 
-          if(Serial.read()=='f') //finish reading
-          {
-            digitalWrite(5, HIGH);
-            device_list[i][1] = device_type;
-            device_list[i][2] = online;
-            delay(100);
-            digitalWrite(5, LOW);
-          }
-         
-       }       
-     }
-     else{
-          device_list[i][2] = offline;
-     }
-    
-    
-  }
-  digitalWrite(rx_tx_pin, LOW);
-}
+
 
 
 
@@ -187,8 +268,15 @@ void get_controller_id(){
       //controller_alias = EEPROM.get(alias_eeprom_addr);
       //Serial.print("controller alias: TODO");
       //Serial.println(controller_alias);
+     
       reconnect();
       client.loop();
+      device_state = 2;
+      char topic[20];
+      sprintf(topic, "%s%d%s", mqtt_device_topic,controller_uuid,mqtt_ping_topic);
+      client.subscribe(topic);
+      sprintf(topic, "%s%d%s", mqtt_device_topic,controller_uuid,mqtt_sync_topic);
+      client.subscribe(topic);
      }
    else{
       
@@ -208,15 +296,23 @@ void get_controller_id(){
 }
 
 
-void update_button(int id, int button_id){
-  Serial.println(id);
+void press_button(int id, int button_id){
+  //Serial.println(id);
 
-  Serial.print(button_id);
+  //Serial.print(button_id);
   char topic[30];
-  sprintf(topic, "%sdevices/%d/buttons/%d", mqtt_device_topic, id, button_id);
+  sprintf(topic, "%s%d/devices/%d/buttons/%d", mqtt_device_topic,controller_uuid, id, button_id);
   
   client.publish(topic, "pressed");
-  delay(10);
+  
+}  
+void release_button(int id, int button_id){
+  //Serial.println(id);
+
+  //Serial.print(button_id);
+  char topic[30];
+  sprintf(topic, "%s%d/devices/%d/buttons/%d", mqtt_device_topic,controller_uuid, id, button_id);
+  
   client.publish(topic, "released");
 }  
 
@@ -252,74 +348,56 @@ void loop() {
   if (!client.connected()) {  reconnect();    }
 
   if (sync_button == true) {
+    Serial.println("syncing");
     sync();
   }
+
+  if(ping_pending){
+    char topic[20];
+    for(int device_id=1; device_id<5; device_id++){
+    sprintf(topic, "%s%d/ping/device_%d/type", mqtt_device_topic,controller_uuid, device_id);
+    if((device_list[device_id][1]) == 0){client.publish(topic,"unasigned");}
+    if((device_list[device_id][1]) == 1){client.publish(topic,"keypad");}
+    
+    sprintf(topic, "%s%d/ping/device_%d/state", mqtt_device_topic,controller_uuid, device_id);
+    if((device_list[device_id][2]) == 0){client.publish(topic,"offline");}
+    if((device_list[device_id][2]) == 1){client.publish(topic,"online");}
+
+    }
+    ping_pending = false;
+  }
+
   
   digitalWrite(rx_tx_pin, LOW);
   if ( Serial.available ()) {
-      Serial.print("why");
       if ( Serial.read () == 'i' ) {
         int device_id = Serial.parseInt ();
-        Serial.print(device_id);
         
         
         char function = Serial.read ();
-        if (function == 'u' ) {
+        if (function == 'p' ) {
             int button_id = Serial.parseInt ();
-            Serial.print(button_id);
+            
             if ( Serial.read () == 'f' ) {
-                      update_button(device_id,button_id);
-                      Serial.print("recieved");
+                      press_button(device_id,button_id);
+                      Serial.print("Button ");
+                      Serial.print(button_id);
+                      Serial.println(" pressed.");
                       }
                     }
-                }
-     
-}
-}
-
-void new_device() {
-  int i = 1;
-  ping_devices();
- 
-  while(i < 17){                                                              //////////////////
-      if(device_list[i][2] == offline){
-        int id = i;
-        Serial.print('I');
-        Serial.print('N');
-        Serial.print('P');
-        Serial.print(id);
-        Serial.print('F');
-        break;
-        }
-      ping_devices();
-  }
-  
-}
-
-void sync() {
-  
-  Serial.print('I'); //initiate data packet
-  Serial.print('Z'); //code for unassigned
-  Serial.print('S'); //code for Sync
-  Serial.print('F'); //finish data packet
-  Serial.flush();    
-  
-  digitalWrite(rx_tx_pin, LOW);
-  
-  if(Serial.find('i')) {
-      if(Serial.read() =='z') {
-          if(Serial.read() =='n') {
-              if(Serial.read() =='n') {
-                  if(Serial.read()=='f') //finish reading
-                      {
                 
-                         digitalWrite(rx_tx_pin, HIGH);
-                         new_device(); 
+         else if (function == 'r' ) {
+            int button_id = Serial.parseInt ();
+            
+            if ( Serial.read () == 'f' ) {
+                      release_button(device_id,button_id);
+                      Serial.print("Button ");
+                      Serial.print(button_id);
+                      Serial.println(" released.");
                       }
-              } 
-          }
-      
-      }
-
-  }
+                    }
+                
+        
+}
+}
 }
